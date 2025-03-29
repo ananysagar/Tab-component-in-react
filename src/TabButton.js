8633aa0cc9b0acad9cbaa963c3546cc8c880263b
ananysagar <sagaranany577@gmail.com>
2025-03-29 22:35:01 +0530

    adding active css for active buttons

--- src/TabButton.js
+++ src/TabButton.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 
 function TabButton({ richestPerson, activeTab, setActiveTab }) {
+  
   return (
     <div className="tab-header">
       {richestPerson.map((person, index) => {
         return (
           <div
             key={person.name}
-            className="tab-button"
+            className={`tab-button ${activeTab===index ? 'active' : ''}`}
             onClick={() => setActiveTab(index)}
           >
             {person.name}
